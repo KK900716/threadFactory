20016f0597473100b86711550c7bb5cb6e4f578a
HPME <443808626@qq.com>
2022-10-16 11:25:29 +0800

    feat:thread Factory

--- src/main/java/com/example/main/ExecuteFactory.java
+++ src/main/java/com/example/main/ExecuteFactory.java
@@ -20,19 +20,30 @@ public final class ExecuteFactory {
     private final List<ReentrantLock> exeLocks;
     private final List<Condition> exeConditions;
 
+    /**
+     * 构建线程执行工厂。
+     *
+     * @param threadNum 线程数
+     */
     public ExecuteFactory(final int threadNum) {
         this.threadNum = threadNum;
         executorService = Executors.newFixedThreadPool(threadNum);
+        // 提交要按序提交，建立一把提交锁
         final ReentrantLock commitLock = new ReentrantLock();
+        // 各线程间要按需通知依次提交，构建条件
         final Condition commitCondition = commitLock.newCondition();
+        // 建立一个全局可用的标志变量，标识当前执行的线程
         final AtomicInteger exeSignal = new AtomicInteger(0);
+        // 每个线程的执行必须等待该线程上个任务执行结束才能执行，构建一组执行锁
         exeLocks = new ArrayList<>(threadNum);
+        // 构建一组线程执行条件，用于通知该不该执行下一个任务
         exeConditions = new ArrayList<>(threadNum);
         for (int i = 0; i < threadNum; i++) {
             final ReentrantLock exeLock = new ReentrantLock();
             final Condition exeCondition = exeLock.newCondition();
             exeLocks.add(exeLock);
             exeConditions.add(exeCondition);
+            // 构建并执行线程
             executorService.execute(new Execute(commitLock, commitCondition, exeLock, exeCondition, exeSignal,
                     i, threadNum));
         }
@@ -42,11 +53,21 @@ public final class ExecuteFactory {
         this(DEFAULT_THREAD_NUM);
     }
 
-    public synchronized void execute() {
-        exeLocks.get(exeId).lock();
-        exeConditions.get(exeId).signalAll();
-        exeLocks.get(exeId).unlock();
-        exeId = exeId == threadNum - 1 ? 0 : exeId + 1;
+    /**
+     * 执行一个任务，每次调用该方法必须是互斥的。
+     */
+    public void execute() {
+        int exeIdValue;
+        // 保证下一次执行execute方法是针对与下一个线程
+        synchronized (this) {
+            exeIdValue = exeId;
+            exeId = exeId == threadNum - 1 ? 0 : exeId + 1;
+        }
+        // 当该该线程未执行完，则阻塞当前执行
+        exeLocks.get(exeIdValue).lock();
+        // 通知该线程执行
+        exeConditions.get(exeIdValue).signalAll();
+        exeLocks.get(exeIdValue).unlock();
     }
 
     public void shutdown() {
--- src/main/java/com/example/main/Main.java
+++ src/main/java/com/example/main/Main.java
@@ -10,11 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        exe();
-    }
-    public static void exe() {
+        // 创建线程执行工厂，传入线程数，Connection[]
         ExecuteFactory executeFactory = new ExecuteFactory();
         for (int i = 0; i < 5000000; i++) {
+            // 模拟流源源不断到来执行
             executeFactory.execute();
             log.info("{}",i);
         }
